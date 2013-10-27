2f6efedd3a8dda11aa03310be343dcefca1d8cb9
ssdns <ssdnbys@gmail.com>
2013-10-27 14:36:28 +0900

    Determine what version of Capistrano gem is being used

--- lib/capistrano-unicorn.rb
+++ lib/capistrano-unicorn.rb
@@ -1,3 +1,8 @@
 require 'capistrano/version'
 require 'capistrano-unicorn/version'
-require 'capistrano-unicorn/capistrano_integration'
+if defined?(Capistrano::VERSION) &&
+    Gem::Version.new(Capistrano::VERSION).release >= Gem::Version.new('3.0.0')
+  load "capistrano-unicorn/tasks/capistrano_integration.rake"
+else
+  require 'capistrano-unicorn/capistrano_integration'
+end
